20e71929f4eecae26cc4f38df605d71be5e6dda1
DKislicyn <DKislicyn@beeline.kz>
2025-08-26 17:35:34 +0300

    ver. 0.0.2 - line fixes

--- src/screens/game/modules/line.lua
+++ src/screens/game/modules/line.lua
@@ -7,6 +7,18 @@ local v_length = vmath.length
 local line_in_drawing = nil
 local t_insert = table.insert
 
+local window_h, window_w = 1280, 720
+local screen_h, screen_w = 1280, 720
+
+local function calc_fit_h()
+	return (screen_h/screen_w)*window_w
+end
+
+local function calc_fit_w()
+	return (screen_w/screen_h)*window_h
+end
+local fit_h, fit_w = calc_fit_h(), calc_fit_w()
+
 local function cubic_bezier_points(a, handle_a, b, handle_b, step)
 	line_points = {}
 	t_insert(line_points, a)
@@ -60,17 +72,19 @@ end
 function Line.new(context, dot_node)
 	local self = setmetatable({}, Line)
 
-	self.root = gui.get_node('input_letters_root')
-	self.action_delta = gui.get_screen_position(self.root)
+	self.root = gui.get_node('line')
 	self.dot_node = dot_node
 	self.segments = {}
 	self.context = context
+
+	self:on_resize()
 
 	return self
 end
 
 function Line:on_resize()
-	self.action_delta = gui.get_screen_position(self.root)
+	screen_w, screen_h = window.get_size()
+	fit_h, fit_w = calc_fit_h(), calc_fit_w()
 end
 
 function Line:clear()
@@ -82,18 +96,21 @@ function Line:clear()
 	self.segments = {}
 end
 
-function Line:add(start_point)
-	-- дорисовываем старый путь если есть
-	if #self.segments > 0 then
-		set_dots(self, self.segments[#self.segments], start_point)
-	end
-	
+function Line:add(start_point_node)
 	local segment = {}
-	segment.start_point = start_point
+	segment.start_point_node = start_point_node
+	segment.start_point = gui.screen_to_local(self.root, gui.get_screen_position(segment.start_point_node))
+	segment.start_point.x = segment.start_point.x * math.max(1, fit_w/window_w)
+	segment.start_point.y = segment.start_point.y * math.max(1, fit_h/window_h)
 	segment.length = 0
 	segment.dots = {}
 	segment.handle_start =  vmath.vector3()
 	segment.handle_press = vmath.vector3()
+
+	-- дорисовываем старый путь если есть
+	if #self.segments > 0 then
+		set_dots(self, self.segments[#self.segments], segment.start_point)
+	end
 
 	t_insert(self.segments, segment)
 end
@@ -108,9 +125,8 @@ end
 function Line:draw(action)
 	-- only last line
 	line_in_drawing = self.segments[#self.segments]
-	press_point.x = action.screen_x
-	press_point.y = action.screen_y
-	press_point = press_point - self.action_delta
+	press_point.x = action.x * math.max(1, fit_w/window_w)
+	press_point.y = action.y * math.max(1, fit_h/window_h)
 
 	if #self.segments > 1 then
 		self.segments[#self.segments-1].handle_press = -(press_point - self.segments[#self.segments-1].start_point) / 5
@@ -129,6 +145,5 @@ function Line:on_input(action)
 		end
 	end
 end
-
 
 return Line
--- src/screens/screens.lua
+++ src/screens/screens.lua
@@ -1,4 +1,5 @@
 return {
 	game = hash('game'),
-	win_screen = hash('win_screen')
+	win_screen = hash('win_screen'),
+	reload_game_screen = hash('reload_game_screen')
 }
